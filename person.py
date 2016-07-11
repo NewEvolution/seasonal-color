0d0f52c7a393ab593871f75a3c1b647a8167e436
Ryan Tanay <ryantanay@newevolution.org>
2016-07-11 15:51:29 -0500

    Added season property and stub methods

--- person.py
+++ person.py
@@ -3,3 +3,10 @@ class Person:
     def __init__(self):
         self.skin_tone = ''
         self.skin_undertone = ''
+        self.season = ''
+
+    def match_color_to_season(self, color):
+        print(color)
+
+    def match_season_to_palette(self, season):
+        print(season)
