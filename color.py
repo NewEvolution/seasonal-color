928ca5001e65130b8d3b9cff10b3b1ac55a9ec88
Ryan Tanay <ryantanay@newevolution.org>
2016-07-11 15:30:45 -0500

    Removed duplicate instantiation, added color as argument

--- color.py
+++ color.py
@@ -3,33 +3,22 @@ import unittest
 class TestPerson:
 
     def test_skin_tone_default_value_is_empty_string(self):
-        a_person = Person()
-        self.assertEqual(a_person.skin_tone, '')
+        self.assertEqual(self.a_person.skin_tone, '')
 
     def test_skin_undertone_default_value_is_empty_string(self):
-        a_person = Person()
-        self.assertEqual(a_person.skin_undertone, '')
+        self.assertEqual(self.a_person.skin_undertone, '')
 
     def test_season_value_is_empty_string(self):
-        a_person = Person()
-        self.assertEqual(a_person.season, '')
+        self.assertEqual(self.a_person.season, '')
 
     def test_match_yellow_to_winter(self):
-        a_person = Person()
-        a_person.skin_undertone = 'yellow'
-        self.assertEqual(a_person.match_color_to_season(), 'winter')
+        self.assertEqual(self.a_person.match_color_to_season('yellow'), 'winter')
 
     def test_match_pink_to_spring(self):
-        a_person = Person()
-        a_person.skin_undertone = 'pink'
-        self.assertEqual(a_person.match_color_to_season(), 'spring')
+        self.assertEqual(self.a_person.match_color_to_season('pink'), 'spring')
 
     def test_match_blue_to_summer(self):
-        a_person = Person()
-        a_person.skin_undertone = 'blue'
-        self.assertEqual(a_person.match_color_to_season(), 'summer')
+        self.assertEqual(self.a_person.match_color_to_season('blue'), 'summer')
 
     def test_match_green_to_fall(self):
-        a_person = Person()
-        a_person.skin_undertone = 'green'
-        self.assertEqual(a_person.match_color_to_season(), 'fall')
+        self.assertEqual(self.a_person.match_color_to_season('green'), 'fall')
