61898f9fa5c0435c575a367469dfc9c7ad02e9ac
Ryan Tanay <ryantanay@newevolution.org>
2016-07-11 16:04:42 -0500

    Fleshed out color/season matching methods

--- person.py
+++ person.py
@@ -6,7 +6,20 @@ class Person:
         self.season = ''
 
     def match_color_to_season(self, color):
-        print(color)
+        pairings = {
+                'yellow': 'winter',
+                'pink': 'spring',
+                'blue': 'summer',
+                'green': 'fall'
+                }
+        self.season = pairings[color]
+        return self.season
 
     def match_season_to_palette(self, season):
-        print(season)
+        pairings = {
+                'winter': ['325C', '608D', '323A'],
+                'spring': ['536F', '847K', '243A'],
+                'summer': ['948G', '915T', '256F'],
+                'fall': ['822S', '363G', '543K']
+                }
+        return pairings[season]
